2f63f69e7a1ff95f2afd3cb9861f80c22bfe61bb
hugosenari <hugosenari@gmail.com>
2022-03-18 18:32:10 -0300

    chore: re index fusion

--- flake.nix
+++ flake.nix
@@ -7,6 +7,30 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."fusion-master".dir   = "master";
+  inputs."fusion-master".owner = "nim-nix-pkgs";
+  inputs."fusion-master".ref   = "master";
+  inputs."fusion-master".repo  = "fusion";
+  inputs."fusion-master".type  = "github";
+  inputs."fusion-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fusion-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."fusion-v1_0".dir   = "v1_0";
+  inputs."fusion-v1_0".owner = "nim-nix-pkgs";
+  inputs."fusion-v1_0".ref   = "master";
+  inputs."fusion-v1_0".repo  = "fusion";
+  inputs."fusion-v1_0".type  = "github";
+  inputs."fusion-v1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fusion-v1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."fusion-v1_1".dir   = "v1_1";
+  inputs."fusion-v1_1".owner = "nim-nix-pkgs";
+  inputs."fusion-v1_1".ref   = "master";
+  inputs."fusion-v1_1".repo  = "fusion";
+  inputs."fusion-v1_1".type  = "github";
+  inputs."fusion-v1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fusion-v1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
